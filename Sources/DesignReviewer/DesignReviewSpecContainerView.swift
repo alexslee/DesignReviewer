0c5bf98c76d6cfb7663414e183b6fe5ae1b3a750
alexslee <alexlee97@outlook.com>
2022-08-05 23:56:18 -0400

    TRASH-12: new method to hopefully refine position of spec labels
    - also drawing a line between the edges for which the spec label is displaying the gap value, to hopefully make it even more evident which distance is being shown

--- Sources/DesignReviewer/DesignReviewSpecContainerView.swift
+++ Sources/DesignReviewer/DesignReviewSpecContainerView.swift
@@ -23,6 +23,7 @@ class DesignReviewSpecContainerView: DesignReviewSelectableView {
   private var recentBottomConstraints = [NSLayoutConstraint]()
   private var recentLeftConstraints = [NSLayoutConstraint]()
   private var recentRightConstraints = [NSLayoutConstraint]()
+  private var pathViews = [UIView]()
 
   // MARK: - Spec views
 
@@ -78,12 +79,18 @@ class DesignReviewSpecContainerView: DesignReviewSelectableView {
     let secondaryFrame = secondaryView?.convert(secondaryView?.bounds ?? .zero, to: containerView) ?? .zero
     let specs = DesignReviewViewModel.specs(between: primaryFrame, and: secondaryFrame, in: bounds)
 
+    for view in pathViews {
+      view.removeFromSuperview()
+    }
+
+    pathViews = []
+
     if specs.shouldHideSpec(for: .top) {
       topSpecView.removeFromSuperview()
     } else {
       topSpecView.updateSpec(specs.top)
       recentTopConstraints.forEach({ $0.isActive = false })
-      addSpecViewToContainer(topSpecView, side: .top)
+      addSpecViewToContainerV2(topSpecView, side: .top)
     }
 
     if specs.shouldHideSpec(for: .bottom) {
@@ -91,7 +98,7 @@ class DesignReviewSpecContainerView: DesignReviewSelectableView {
     } else {
       bottomSpecView.updateSpec(specs.bottom)
       recentBottomConstraints.forEach({ $0.isActive = false })
-      addSpecViewToContainer(bottomSpecView, side: .bottom)
+      addSpecViewToContainerV2(bottomSpecView, side: .bottom)
     }
 
     if specs.shouldHideSpec(for: .left) {
@@ -99,7 +106,7 @@ class DesignReviewSpecContainerView: DesignReviewSelectableView {
     } else {
       leftSpecView.updateSpec(specs.left)
       recentLeftConstraints.forEach({ $0.isActive = false })
-      addSpecViewToContainer(leftSpecView, side: .left)
+      addSpecViewToContainerV2(leftSpecView, side: .left)
     }
 
     if specs.shouldHideSpec(for: .right) {
@@ -107,12 +114,162 @@ class DesignReviewSpecContainerView: DesignReviewSelectableView {
     } else {
       rightSpecView.updateSpec(specs.right)
       recentRightConstraints.forEach({ $0.isActive = false })
-      addSpecViewToContainer(rightSpecView, side: .right)
+      addSpecViewToContainerV2(rightSpecView, side: .right)
     }
   }
 
   // what the actual hell have you done...
 
+  private func addSpecViewToContainerV2(_ specView: UIView, side: Specs.Side) {
+    guard let containerView = containerView else { return }
+
+    containerView.addSubview(specView)
+
+    guard let primaryFrame = primaryView?.convert(primaryView?.bounds ?? .zero, to: containerView),
+          let secondaryFrame = secondaryView?.convert(secondaryView?.bounds ?? .zero, to: containerView) else {
+        return
+    }
+
+    var centerPoint: CGPoint?
+    var path: UIBezierPath?
+
+    /*
+     Figure out where to place the spec label depending on which sides we are comparing, and prepare the path for the
+     line that will be drawn between the sides.
+     */
+    switch side {
+    case .top:
+      let topOfPrimary = CGPoint(x: primaryFrame.origin.x + primaryFrame.size.width / 2, y: primaryFrame.origin.y)
+
+      if isFrame(secondaryFrame, inside: primaryFrame) {
+        // secondary view is entirely inside the primary view
+        let topOfSecondary = CGPoint(x: primaryFrame.origin.x + primaryFrame.size.width / 2, y: secondaryFrame.origin.y)
+        centerPoint = CGPoint(x: topOfSecondary.x, y: topOfPrimary.y + ((topOfSecondary.y - topOfPrimary.y) / 2))
+
+        path = makePath(from: topOfPrimary, to: topOfSecondary)
+      } else if primaryFrame.origin.y + primaryFrame.size.height < secondaryFrame.origin.y {
+        // primary view is entirely above the secondary view
+        let bottomOfPrimary = CGPoint(x: primaryFrame.origin.x + primaryFrame.size.width / 2,
+                                      y: primaryFrame.origin.y + primaryFrame.size.height)
+        let topOfSecondary = CGPoint(x: primaryFrame.origin.x + primaryFrame.size.width / 2, y: secondaryFrame.origin.y)
+        centerPoint = CGPoint(x: topOfSecondary.x, y: bottomOfPrimary.y + ((topOfSecondary.y - bottomOfPrimary.y) / 2))
+
+        path = makePath(from: bottomOfPrimary, to: topOfSecondary)
+      } else {
+        let topOfSecondary = CGPoint(x: primaryFrame.origin.x + primaryFrame.size.width / 2, y: secondaryFrame.origin.y)
+        centerPoint = CGPoint(x: topOfSecondary.x, y: topOfPrimary.y + ((topOfSecondary.y - topOfPrimary.y) / 2))
+
+        path = makePath(from: topOfPrimary, to: topOfSecondary)
+      }
+
+      guard let center = centerPoint else { return }
+
+      recentTopConstraints = [
+        specView.centerXAnchor.constraint(equalTo: containerView.leadingAnchor, constant: center.x),
+        specView.centerYAnchor.constraint(equalTo: containerView.topAnchor, constant: center.y)]
+      NSLayoutConstraint.activate(recentTopConstraints)
+    case .left:
+      let leftOfPrimary = CGPoint(x: primaryFrame.origin.x, y: primaryFrame.origin.y + primaryFrame.size.height / 2)
+
+      if isFrame(primaryFrame, inside: secondaryFrame) {
+        // primary view is entirely inside the secondary view
+        let leftOfSecondary = CGPoint(x: secondaryFrame.origin.x, y: leftOfPrimary.y)
+        centerPoint = CGPoint(x: leftOfSecondary.x + ((leftOfPrimary.x - leftOfSecondary.x) / 2), y: leftOfSecondary.y)
+
+        path = makePath(from: leftOfPrimary, to: leftOfSecondary)
+      } else if primaryFrame.origin.x + primaryFrame.size.width < secondaryFrame.origin.x {
+        // primary view is entirely left of the secondary view
+        let leftOfSecondary = CGPoint(x: secondaryFrame.origin.x, y: leftOfPrimary.y)
+        let rightOfPrimary = CGPoint(x: primaryFrame.origin.x + primaryFrame.size.width,
+                                     y: primaryFrame.origin.y + primaryFrame.size.height / 2)
+        centerPoint = CGPoint(x: rightOfPrimary.x + ((leftOfSecondary.x - rightOfPrimary.x) / 2), y: leftOfSecondary.y)
+
+        path = makePath(from: rightOfPrimary, to: leftOfSecondary)
+      } else {
+        let leftOfSecondary = CGPoint(x: secondaryFrame.origin.x, y: leftOfPrimary.y)
+        centerPoint = CGPoint(x: leftOfSecondary.x + ((leftOfPrimary.x - leftOfSecondary.x) / 2), y: leftOfSecondary.y)
+
+        path = makePath(from: leftOfPrimary, to: leftOfSecondary)
+      }
+
+      guard let center = centerPoint else { return }
+
+      recentLeftConstraints = [
+        specView.centerXAnchor.constraint(equalTo: containerView.leadingAnchor, constant: center.x),
+        specView.centerYAnchor.constraint(equalTo: containerView.topAnchor, constant: center.y)]
+      NSLayoutConstraint.activate(recentLeftConstraints)
+    case .bottom:
+      let bottomOfPrimary = CGPoint(x: primaryFrame.origin.x + (primaryFrame.size.width / 2), y: primaryFrame.origin.y + primaryFrame.size.height)
+      if isFrame(primaryFrame, inside: secondaryFrame) {
+        // primary view is entirely inside the secondary view
+        let bottomOfSecondary = CGPoint(x: bottomOfPrimary.x, y: secondaryFrame.origin.y + secondaryFrame.size.height)
+
+        centerPoint = CGPoint(x: bottomOfSecondary.x, y: bottomOfPrimary.y + ((bottomOfSecondary.y - bottomOfPrimary.y) / 2))
+
+        path = makePath(from: bottomOfPrimary, to: bottomOfSecondary)
+      } else if bottomOfPrimary.y <= secondaryFrame.origin.y {
+        // primary view is entirely above the secondary view
+        let topOfSecondary = CGPoint(x: bottomOfPrimary.x, y: secondaryFrame.origin.y)
+        centerPoint = CGPoint(x: topOfSecondary.x, y: bottomOfPrimary.y + ((topOfSecondary.y - bottomOfPrimary.y) / 2))
+
+        path = makePath(from: bottomOfPrimary, to: topOfSecondary)
+      } else if primaryFrame.origin.y > secondaryFrame.origin.y + secondaryFrame.size.height {
+        // primary view is entirely below the secondary view
+        let bottomOfSecondary = CGPoint(x: bottomOfPrimary.x, y: secondaryFrame.origin.y + secondaryFrame.size.height)
+        let topOfPrimary = CGPoint(x: primaryFrame.origin.x + (primaryFrame.size.width / 2), y: primaryFrame.origin.y)
+        centerPoint = CGPoint(x: bottomOfSecondary.x, y: bottomOfSecondary.y + ((topOfPrimary.y - bottomOfSecondary.y) / 2))
+
+        path = makePath(from: bottomOfSecondary, to: topOfPrimary)
+      } else {
+        let bottomOfSecondary = CGPoint(x: bottomOfPrimary.x, y: secondaryFrame.origin.y + secondaryFrame.size.height)
+
+        centerPoint = CGPoint(x: bottomOfSecondary.x, y: bottomOfPrimary.y + ((bottomOfSecondary.y - bottomOfPrimary.y) / 2))
+
+        path = makePath(from: bottomOfPrimary, to: bottomOfSecondary)
+      }
+
+      guard let center = centerPoint else { return }
+
+      recentBottomConstraints = [
+        specView.centerXAnchor.constraint(equalTo: containerView.leadingAnchor, constant: center.x),
+        specView.centerYAnchor.constraint(equalTo: containerView.topAnchor, constant: center.y)]
+      NSLayoutConstraint.activate(recentBottomConstraints)
+    case .right:
+      let rightOfPrimary = CGPoint(x: primaryFrame.origin.x + primaryFrame.size.width,
+                                   y: primaryFrame.origin.y + primaryFrame.size.height / 2)
+      if isFrame(primaryFrame, inside: secondaryFrame) {
+        // primary view is entirely inside the secondary view
+        let rightOfSecondary = CGPoint(x: secondaryFrame.origin.x + secondaryFrame.size.width, y: rightOfPrimary.y)
+        centerPoint = CGPoint(x: rightOfPrimary.x + ((rightOfSecondary.x - rightOfPrimary.x) / 2), y: rightOfPrimary.y)
+
+        path = makePath(from: rightOfPrimary, to: rightOfSecondary)
+      } else if primaryFrame.origin.x >= secondaryFrame.origin.x + secondaryFrame.size.width {
+        // primary view is entirely right of the secondary view
+        let leftOfPrimary = CGPoint(x: primaryFrame.origin.x, y: primaryFrame.origin.y + primaryFrame.size.height / 2)
+        let rightOfSecondary = CGPoint(x: secondaryFrame.origin.x + secondaryFrame.size.width, y: leftOfPrimary.y)
+        centerPoint = CGPoint(x: rightOfSecondary.x + ((leftOfPrimary.x - rightOfSecondary.x) / 2), y: leftOfPrimary.y)
+
+        path = makePath(from: rightOfSecondary, to: leftOfPrimary)
+      } else {
+        let rightOfSecondary = CGPoint(x: secondaryFrame.origin.x + secondaryFrame.size.width, y: rightOfPrimary.y)
+        centerPoint = CGPoint(x: rightOfSecondary.x + ((rightOfPrimary.x - rightOfSecondary.x) / 2), y: rightOfPrimary.y)
+
+        path = makePath(from: rightOfSecondary, to: rightOfPrimary)
+      }
+
+      guard let center = centerPoint else { return }
+
+      recentRightConstraints = [
+        specView.centerXAnchor.constraint(equalTo: containerView.leadingAnchor, constant: center.x),
+        specView.centerYAnchor.constraint(equalTo: containerView.topAnchor, constant: center.y)]
+      NSLayoutConstraint.activate(recentRightConstraints)
+    }
+
+    // lastly, draw the line between the two edges
+    if let drawPath = path { addShapeLayer(for: drawPath) }
+  }
+
+  // TODO: delete this once proven that the new method is reliable
   private func addSpecViewToContainer(_ specView: UIView, side: Specs.Side) {
     guard let containerView = containerView else { return }
 
@@ -254,5 +411,76 @@ class DesignReviewSpecContainerView: DesignReviewSelectableView {
         specView.centerYAnchor.constraint(equalTo: centerYBaseAnchor)]
       NSLayoutConstraint.activate(recentRightConstraints)
     }
+  }
+
+  private func isFrame(_ candidateFrame: CGRect, inside comparisonFrame: CGRect) -> Bool {
+    return candidateFrame.origin.x >= comparisonFrame.origin.x &&
+    candidateFrame.origin.x + candidateFrame.size.width <= comparisonFrame.origin.x + comparisonFrame.size.width &&
+    candidateFrame.origin.y >= comparisonFrame.origin.y &&
+    candidateFrame.origin.y + candidateFrame.size.height <= comparisonFrame.origin.y + comparisonFrame.size.height
+  }
+
+  // creates the path between two given points, meant to turn into a CAShapeLayer via `addShapeLayer(for:)`
+  private func makePath(from start: CGPoint, to end: CGPoint) -> UIBezierPath {
+    let retVal = UIBezierPath()
+
+    let isVertical = start.y != end.y
+
+    if isVertical {
+      let isStartLessThanEnd = start.y < end.y
+
+      let verticalStart = start.y + (.extraExtraSmall * (isStartLessThanEnd ? 1 : -1))
+      let verticalEnd = end.y + (.extraExtraSmall * (isStartLessThanEnd ? -1 : 1))
+
+      retVal.move(to: CGPoint(x: start.x - .extraExtraSmall, y: verticalStart))
+      retVal.addLine(to: CGPoint(x: start.x + .extraExtraSmall, y: verticalStart))
+      retVal.addLine(to: CGPoint(x: start.x, y: verticalStart))
+      retVal.addLine(to: CGPoint(x: end.x, y: verticalEnd))
+      retVal.addLine(to: CGPoint(x: end.x - .extraExtraSmall, y: verticalEnd))
+      retVal.addLine(to: CGPoint(x: end.x + .extraExtraSmall, y: verticalEnd))
+      retVal.addLine(to: CGPoint(x: end.x, y: verticalEnd))
+      retVal.addLine(to: CGPoint(x: start.x, y: verticalStart))
+    } else {
+      let isStartLessThanEnd = start.x < end.x
+
+      let horizontalStart = start.x + (.extraExtraSmall * (isStartLessThanEnd ? 1 : -1))
+      let horizontalEnd = end.x + (.extraExtraSmall * (isStartLessThanEnd ? -1 : 1))
+
+      retVal.move(to: CGPoint(x: horizontalStart, y: start.y))
+      retVal.addLine(to: CGPoint(x: horizontalStart, y: start.y - .extraExtraSmall))
+      retVal.addLine(to: CGPoint(x: horizontalStart, y: start.y + .extraExtraSmall))
+      retVal.addLine(to: CGPoint(x: horizontalStart, y: start.y))
+      retVal.addLine(to: CGPoint(x: horizontalEnd, y: end.y))
+      retVal.addLine(to: CGPoint(x: horizontalEnd, y: end.y - .extraExtraSmall))
+      retVal.addLine(to: CGPoint(x: horizontalEnd, y: end.y + .extraExtraSmall))
+      retVal.addLine(to: CGPoint(x: horizontalEnd, y: end.y))
+      retVal.move(to: CGPoint(x: horizontalStart, y: start.y))
+    }
+
+    // the line should end up looking like: |-----(spec view)-----| (or the vertical version, if isVertical was true)
+    return retVal
+  }
+
+  /// draws the line between the edges of the views to help make it clear which gap the spec refers to
+  private func addShapeLayer(for path: UIBezierPath) {
+    guard let containerView = containerView else { return }
+    let shapeLayer = CAShapeLayer()
+    shapeLayer.bounds = containerView.bounds
+    shapeLayer.position = containerView.center
+    shapeLayer.path = path.cgPath
+
+    shapeLayer.lineWidth = 2
+    shapeLayer.borderColor = UIColor.primary3.cgColor
+    shapeLayer.fillColor = UIColor.primary3.cgColor
+    shapeLayer.strokeColor = UIColor.primary3.cgColor
+
+    let shapeView = UIView()
+    shapeView.backgroundColor = .clear
+    shapeView.layer.addSublayer(shapeLayer)
+    shapeView.translatesAutoresizingMaskIntoConstraints = false
+
+    containerView.addSubview(shapeView)
+    NSLayoutConstraint.activate(shapeView.constraints(toView: containerView))
+    pathViews.append(shapeView)
   }
 }
